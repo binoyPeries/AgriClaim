66198e2ccb11f98923d104389ff32169f73dc676
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-12 20:20:49 +0530

    Color pallet added

---
+++ lib/ui/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class AgriClaimColors {
+  static const Color primaryColor = Color(0xFF00972C);
+  static const Color secondaryColor = Color(0xFF1AC95A);
+  static const Color tertiaryColor = Color(0xFF005A97);
+
+  static MaterialColor primaryMaterialColor =
+      MaterialColor(primaryColor.value, const {
+    50: Color.fromRGBO(0, 151, 44, .1),
+    100: Color.fromRGBO(0, 151, 44, .2),
+    200: Color.fromRGBO(0, 151, 44, .3),
+    300: Color.fromRGBO(0, 151, 44, .4),
+    400: Color.fromRGBO(0, 151, 44, .5),
+    500: Color.fromRGBO(0, 151, 44, .6),
+    600: Color.fromRGBO(0, 151, 44, .7),
+    700: Color.fromRGBO(0, 151, 44, .8),
+    800: Color.fromRGBO(0, 151, 44, .9),
+    900: Color.fromRGBO(0, 151, 44, 1),
+  });
+}

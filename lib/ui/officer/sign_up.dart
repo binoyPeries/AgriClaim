7739d6cff77fdd2fb34185e33ecc76eeb13d28ac
sgayangi <gayangi.18@cse.mrt.ac.lk>
2022-12-15 20:48:58 +0530

    refactoring

---
+++ lib/ui/officer/sign_up.dart
@@ -0,0 +1,83 @@
+import 'package:agriclaim/ui/common/components/default_appbar.dart';
+import 'package:agriclaim/ui/common/components/default_scaffold.dart';
+import 'package:agriclaim/ui/common/components/primary_button.dart';
+import 'package:agriclaim/ui/common/form_fields/form_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:sizer/sizer.dart';
+
+class OfficerSignUpPage extends StatelessWidget {
+  const OfficerSignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final formKey = GlobalKey<FormBuilderState>();
+
+    return SafeArea(
+      child: DefaultScaffold(
+        appBar: DefaultAppBar(title: "Sign Up", backButtonVisible: true),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                FormBuilder(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(height: 2.h),
+                      FormTextField(
+                        fieldName: "officerRegNo",
+                        label: "Officer Registration Number",
+                        keyboardType: TextInputType.text,
+                        validators: [FormBuilderValidators.min(1)],
+                      ),
+                      SizedBox(height: 2.h),
+                      FormTextField(
+                        fieldName: "firstName",
+                        label: "First Name",
+                        validators: [FormBuilderValidators.min(1)],
+                      ),
+                      SizedBox(height: 2.h),
+                      FormTextField(
+                        fieldName: "lastName",
+                        label: "Last Name",
+                        validators: [FormBuilderValidators.min(1)],
+                      ),
+                      SizedBox(height: 2.h),
+                      FormTextField(
+                        fieldName: "email",
+                        label: "Email Address",
+                        validators: [
+                          FormBuilderValidators.email(
+                              errorText: "Please enter a valid email address.")
+                        ],
+                      ),
+                      SizedBox(height: 8.h),
+                      PrimaryButton(
+                          onPressed: () => submitLogin(formKey),
+                          text: "Sign Up")
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  bool submitLogin(GlobalKey<FormBuilderState> formKey) {
+    final isValid = formKey.currentState?.saveAndValidate() ?? false;
+    if (!isValid) {
+      return false;
+    }
+    //:TODO signup logic
+    return true;
+  }
+}

b033db443b282014872c1234b3b8d1d575f8b4d7
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-18 15:49:19 +0530

    bottom navbar added

--- lib/ui/farmer/home_screen.dart
+++ lib/ui/farmer/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:agriclaim/ui/common/components/default_scaffold.dart';
+import 'package:agriclaim/ui/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sizer/sizer.dart';
 
 class FarmerHomePage extends StatefulWidget {
   const FarmerHomePage({Key? key}) : super(key: key);
@@ -8,10 +12,63 @@ class FarmerHomePage extends StatefulWidget {
 }
 
 class _FarmerHomePageState extends State<FarmerHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Farmer"),
+    return SafeArea(
+      child: DefaultScaffold(
+        body: Center(
+          child: _pages.elementAt(_selectedIndex),
+        ),
+        bottomNavBar: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          selectedFontSize: 2.h,
+          selectedIconTheme:
+              IconThemeData(color: AgriClaimColors.primaryMaterialColor),
+          selectedItemColor: AgriClaimColors.primaryColor,
+          unselectedFontSize: 15,
+          unselectedIconTheme: const IconThemeData(color: Colors.grey),
+          unselectedItemColor: Colors.grey,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.wpforms),
+              label: 'Claims',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.wheatAwn),
+              label: 'Farms',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.user),
+              label: 'Profile',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+      ),
     );
   }
+
+  static const List<Widget> _pages = <Widget>[
+    Icon(
+      Icons.call,
+      size: 150,
+    ),
+    Icon(
+      Icons.camera,
+      size: 150,
+    ),
+    Icon(
+      Icons.chat,
+      size: 150,
+    ),
+  ];
 }

b2f5f3f19241a2f2044fab889589e71acee7f25b
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-19 13:47:30 +0530

    Farmer model updated

--- lib/models/farmer.dart
+++ lib/models/farmer.dart
@@ -4,14 +4,14 @@ part 'farmer.g.dart';
 
 @JsonSerializable()
 class Farmer {
-  final String uid;
+  final String farmerId;
   final String firstName;
   final String lastName;
   final String nic;
   final String homeAddress;
 
   Farmer({
-    required this.uid,
+    required this.farmerId,
     required this.firstName,
     required this.lastName,
     required this.nic,

fd20447caedac9e42e0563c18bd66cc11a8ac300
sgayangi <gayangi.18@cse.mrt.ac.lk>
2022-12-19 12:25:57 +0530

    view farm rough page added

--- lib/ui/farmer/view_farm.dart
+++ lib/ui/farmer/view_farm.dart
@@ -1,25 +1,37 @@
+import 'package:agriclaim/providers/farm_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../common/components/default_appbar.dart';
 import '../common/components/default_scaffold.dart';
 
-class ViewFarmPage extends StatelessWidget {
+class ViewFarmPage extends ConsumerWidget {
   const ViewFarmPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final streamAsyncValue = ref.watch(farmListProvider);
     return SafeArea(
-      child: DefaultScaffold(
-        appBar: const DefaultAppBar(title: "View Farms"),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              children: [],
-            ),
-          ),
-        ),
+        child: DefaultScaffold(
+      appBar: const DefaultAppBar(
+        title: "Farms",
       ),
-    );
+      body: streamAsyncValue.when(
+        data: (item) {
+          List<ListTile> farms = [];
+          for (var element in item) {
+            farms.add(ListTile(
+              title: Text(element.farmName.toString()),
+              subtitle: Text(element.locations.toString()),
+            ));
+          }
+          return Column(
+            children: farms,
+          );
+        },
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (e, st) => Center(child: Text(e.toString())),
+      ),
+    ));
   }
 }

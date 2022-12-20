1d5ff9d81e8bdeaa208a8fa0504a9ca4914cdb05
sgayangi <gayangi.18@cse.mrt.ac.lk>
2022-12-20 12:43:36 +0530

    format updated

--- lib/ui/common/utils/display_lat_long.dart
+++ lib/ui/common/utils/display_lat_long.dart
@@ -2,5 +2,5 @@ String convertMapToLatLong(Map map) {
   return (map["lat"] == 0 && map["long"] == 0) ||
           (map["lat"] == null && map["long"] == null)
       ? "Enter Location"
-      : "${map["lat"]}, ${map["long"]}";
+      : "Latitude: ${map["lat"]}\nLongitude: ${map["long"]}";
 }

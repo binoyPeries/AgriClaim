78e20a530a81c383ea4d296b7c9a40e1d17fb3aa
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-21 18:48:22 +0530

    claim model added

--- lib/models/claim.dart
+++ lib/models/claim.dart
@@ -1,1 +1,35 @@
-class Claim {}
+import 'package:json_annotation/json_annotation.dart';
+
+part 'claim.g.dart';
+
+@JsonSerializable()
+class Claim {
+  final String claimId;
+  final String claimReference;
+  final String farmerId;
+  final String farmId;
+  final String? farmerNote;
+  final List<String> claimPhotos;
+  final String? claimVideo;
+  final double compensation;
+  final String? officerNote;
+  final String status;
+  final String? assignedOfficer;
+
+  Claim(
+    this.claimId,
+    this.claimReference,
+    this.farmerId,
+    this.farmId,
+    this.farmerNote,
+    this.claimPhotos,
+    this.claimVideo,
+    this.compensation,
+    this.officerNote,
+    this.status,
+    this.assignedOfficer,
+  );
+  factory Claim.fromJson(Map<String, dynamic> json) => _$ClaimFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ClaimToJson(this);
+}

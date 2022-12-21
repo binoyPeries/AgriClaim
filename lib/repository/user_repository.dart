8612bf2a206a438e57ae3abb1743d185990c5315
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-21 17:52:30 +0530

    bug fixed

--- lib/repository/user_repository.dart
+++ lib/repository/user_repository.dart
@@ -10,7 +10,7 @@ class UserRepository {
 
   Future<DocumentReference<Map<String, dynamic>>> addFarmer(
       Map<String, dynamic> userData) async {
-    Map<String, dynamic> data = {"uid": loggedUserId, ...userData};
+    Map<String, dynamic> data = {"farmerId": loggedUserId, ...userData};
     try {
       final result = await _store.collection(DatabaseNames.farmer).add(data);
       return result;

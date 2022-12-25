4bdf94e3dd0b4bb48589c570759bfbc2b24b4331
binoy <binoy.18@cse.mrt.ac.lk>
2022-12-25 16:37:04 +0530

    claims image viewer updated with indicators

--- lib/ui/common/components/claim_image_viewer.dart
+++ lib/ui/common/components/claim_image_viewer.dart
@@ -1,49 +1,71 @@
 import 'package:agriclaim/ui/constants/colors.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
-class ClaimImagesViewer extends StatelessWidget {
+class ClaimImagesViewer extends StatefulWidget {
   final List<String> images;
+
   const ClaimImagesViewer({Key? key, required this.images}) : super(key: key);
 
   @override
+  State<ClaimImagesViewer> createState() => _ClaimImagesViewerState();
+}
+
+class _ClaimImagesViewerState extends State<ClaimImagesViewer> {
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: images.isNotEmpty ? 35.h : 10,
-      child: Padding(
-        padding: const EdgeInsets.all(5.0),
-        child: GridView.builder(
-            physics: const ScrollPhysics(),
-            scrollDirection: Axis.horizontal,
-            itemCount: images.length,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1, mainAxisSpacing: 2.w),
-            itemBuilder: (BuildContext context, int index) {
-              return CachedNetworkImage(
-                imageUrl: images[index],
-                imageBuilder: (context, imageProvider) => Container(
-                  decoration: BoxDecoration(
+      height: widget.images.isNotEmpty ? 100.w : 10,
+      child: GridView.builder(
+          physics: const ScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.images.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1, mainAxisSpacing: 2.w),
+          itemBuilder: (BuildContext context, int index) {
+            return Stack(
+              children: [
+                CachedNetworkImage(
+                  imageUrl: widget.images[index],
+                  imageBuilder: (context, imageProvider) => Container(
+                    decoration: BoxDecoration(
                       image: DecorationImage(
                         image: imageProvider,
                         fit: BoxFit.contain,
                       ),
-                      borderRadius: const BorderRadius.all(Radius.circular(20)),
-                      border:
-                          Border.all(color: AgriClaimColors.secondaryColor)),
+                      color: AgriClaimColors.hintColor.withOpacity(0.2),
+                    ),
+                  ),
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      Center(
+                          child: CircularProgressIndicator(
+                              value: downloadProgress.progress)),
+                  errorWidget: (context, url, error) => Center(
+                      child: Icon(
+                    Icons.error,
+                    size: 3.h,
+                  )),
                 ),
-                progressIndicatorBuilder: (context, url, downloadProgress) =>
-                    Center(
-                        child: CircularProgressIndicator(
-                            value: downloadProgress.progress)),
-                errorWidget: (context, url, error) => Center(
-                    child: Icon(
-                  Icons.error,
-                  size: 3.h,
-                )),
-              );
-            }),
-      ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: DotsIndicator(
+                    dotsCount: widget.images.length,
+                    position: index.toDouble(),
+                    decorator: DotsDecorator(
+                      activeColor: AgriClaimColors.primaryColor,
+                      color: AgriClaimColors.hintColor,
+                      size: Size(1.h, 1.h),
+                      activeSize: Size(1.3.h, 1.3.h),
+                      spacing: EdgeInsets.symmetric(
+                          horizontal: 0.5.w, vertical: 1.h),
+                    ),
+                  ),
+                )
+              ],
+            );
+          }),
     );
   }
 }

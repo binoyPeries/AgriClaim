085bf1cc40d7cb964a55e7b3a9f05ab273bd7b07
binoy <binoy.18@cse.mrt.ac.lk>
2023-01-02 12:25:57 +0530

    video location checking added

--- lib/ui/farmer/create_claim_page.dart
+++ lib/ui/farmer/create_claim_page.dart
@@ -68,6 +68,7 @@ class CreateClaimPage extends ConsumerWidget {
                             if (farm != null) {
                               // to clear the already taken photos, since the farm in changed
                               imageList.clear();
+                              video = null;
                               ref
                                   .read(
                                       claimSelectedFarmLocationsNotifierProvider
@@ -94,9 +95,9 @@ class CreateClaimPage extends ConsumerWidget {
                       fontWeight: FontWeight.w500),
                 ),
                 SizedBox(height: 2.2.h),
-                const PhotoAcceptedInfo(),
+                const MediaAcceptedInfo(),
                 SizedBox(height: 1.h),
-                const PhotoRejectedInfo(),
+                const MediaRejectedInfo(),
                 SizedBox(height: 2.5.h),
                 Text(
                   "Submit maximum of 10 picture that clearly shows the damaged crop area. "
@@ -136,10 +137,17 @@ class CreateClaimPage extends ConsumerWidget {
                   textAlign: TextAlign.justify,
                 ),
                 SizedBox(height: 2.5.h),
-                FormVideoField(
-                  fieldName: "claimVideo",
-                  maxDurationInSec: 5,
-                  setVideoOnParent: setVideo,
+                Consumer(
+                  builder: (context, ref, _) {
+                    List<Map<String, double>> farmLocations =
+                        ref.watch(claimSelectedFarmLocationsNotifierProvider);
+                    return FormVideoField(
+                      fieldName: "claimVideo",
+                      maxDurationInSec: 5,
+                      setVideoOnParent: setVideo,
+                      farmLocations: farmLocations,
+                    );
+                  },
                 ),
                 SizedBox(height: 2.5.h),
                 const FormTextAreaField(
